22229cbd5798d0ddb79a749e7ce9908d02b9596c
Eric <eric.m.lombardo@gmail.com>
2021-08-09 14:48:15 -0400

    commit tree 03d7a37a535602d71e889785e8eb4e0cb7505f48

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/* Add NavLink to importer */
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import About from './About'
+import Home from './Home'
+import Login from './Login'
+import Navbar from './Navbar'
 
-const Home = () => {
-  return (
+
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
-);
+); 
+
+// Create seperate files for each class in index.js
